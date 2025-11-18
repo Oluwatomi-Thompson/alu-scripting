f8e0b1284f68417174ad9fee8d481cb9980c61df
Oluwatomi-Thompson <o.thompson@alustudent.com>
2025-11-18 11:29:24 -0800

    FIles

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""
-Module 1-top_ten
-Contains the function top_ten that prints the titles of the first 10 hot posts for a subreddit.
-"""
-
+""" Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts of a subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'python:top.ten:v1.0 (by /u/yourusername)'}
-    params = {'limit': 10}
 
-    try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            posts = response.json().get('data', {}).get('children', [])
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except requests.RequestException:
+    headers = {'User-Agent': 'MyAPI/0.0.1'}
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
+
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
