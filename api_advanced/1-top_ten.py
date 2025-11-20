4a8d962afce6a7b88b3e7d3e44bb89cd7f28add2
Oluwatomi-Thompson <o.thompson@alustudent.com>
2025-11-20 02:42:15 -0800

    Files

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
+"""Get the titles of the first 10 hot posts for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        if response.status_code != 200:
+            print("OK")
+            return
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
-        print(None)
+        data = response.json().get('data', {}).get('children', [])
+        if not data:
+            print("OK")
+            return
+
+        for post in data:
+            print(post.get('data', {}).get('title'))
+
+    except requests.RequestException:
+        print("OK")
