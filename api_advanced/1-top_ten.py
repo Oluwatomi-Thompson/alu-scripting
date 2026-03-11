20967b20a7cc835a562b6c9e0a2211c5d90648ad
Oluwatomi-Thompson <o.thompson@alustudent.com>
2026-03-11 08:33:45 +0000

    Try 2

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -7,16 +7,21 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints titles of top 10 hot posts for a subreddit"""
+    """Prints the titles of the first 10 hot posts for a given subreddit"""
+    # Try a very generic but established User-Agent
+    headers = {'User-Agent': 'Mozilla/5.0'}
     url = "https://www.reddit.com{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'MyRedditAPI/1.0'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data')
+        children = data.get('children')
+        if children:
             for post in children:
                 print(post.get('data').get('title'))
         else:
             print(None)
-    except Exception:
+    else:
+        # If 302 (fake), 404, or 429 (blocked), print None
         print(None)
