34d8152bfe6c1c7880daaf672f3eef7c747fcbcb
Thompson <o.thompson@alustudent.com>
2026-03-19 18:10:23 +0200

    d2
    Refactor top_ten function to improve readability and error handling.

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-"""Script that fetches 10 hot posts for a given subreddit."""
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print titles of top 10 hot posts of a subreddit."""
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    subreddit_url = "https://reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
+    if response.status_code == 200:
+        json_data = response.json()
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
+    else:
         print(None)
-        return
-
-    data = response.json().get('data', {}).get('children', [])
-
-    for post in data[:10]:
-        print(post.get('data', {}).get('title'))
